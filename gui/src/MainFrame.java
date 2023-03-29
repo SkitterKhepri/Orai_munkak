29110d6a53eb3c0b848a87ce16f3000e7168dc59
SkitterKhepri <106982094+SkitterKhepri@users.noreply.github.com>
2023-03-29 20:03:01 +0200

    Add files via upload

--- gui/src/MainFrame.java
+++ gui/src/MainFrame.java
@@ -18,8 +18,23 @@ public class MainFrame extends JFrame{
         this.setSize(400, 300);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("Első ablak");
+        this.setLayout(null);
 
-        JLabel label01 = new JLabel()
+        JLabel label01 = new JLabel();
+        label01.setText("Szöveg");
+        label01.setBounds(110, 110, 50, 30);
+        this.add(label01);
+
+
+        JTextField textField01 = new JTextField(10);
+        textField01.setBounds(170, 110, 100, 30);
+        this.add(textField01);
+
+
+        JButton button01 = new JButton();
+        button01.setText("Gomb!");
+        button01.setBounds(150, 150, 80, 30);
+        this.add(button01);
 
     }
 
