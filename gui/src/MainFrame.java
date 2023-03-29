cc004f8dfd14693b049ca2b030aef779341d46ff
SkitterKhepri <106982094+SkitterKhepri@users.noreply.github.com>
2023-03-29 19:50:58 +0200

    Add files via upload

---
+++ gui/src/MainFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class MainFrame extends JFrame{
+    
+    MainFrame(){
+
+        initComponents();
+
+    }
+
+    private void initComponents(){
+
+        this.setSize(400, 300);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setTitle("Első ablak");
+
+        JLabel label01 = new JLabel()
+
+    }
+
+}
