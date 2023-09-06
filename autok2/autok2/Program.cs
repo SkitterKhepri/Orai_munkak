47b2b92baa2999d853cacbedcbc8c38cab9bfef7
András Bercsek <berandris@gmail.com>
2023-09-06 19:25:07 +0200

    eret

---
+++ autok2/autok2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok2
+{
+    struct Car
+    {
+
+        int sebesseg;
+        string rendszam;
+
+        public int Sebesseg
+        {
+            get
+            {
+                return sebesseg;
+            }
+
+            set
+            {
+                if (value > 0 && value < 6)
+                {
+                    sebesseg = value;
+                }
+            }
+
+        }
+
+    }
+
+    class Auto
+    {
+
+        int sebesseg;
+        string rendszam;
+        public int Sebesseg
+        {
+            get 
+            { 
+                return sebesseg; 
+            }
+
+            set
+            {
+                if (value > 0 && value < 6)
+                {
+                    sebesseg = value;
+                }
+            }
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car c = new Car();
+
+            Auto a = new Auto();
+
+        }
+    }
+}
