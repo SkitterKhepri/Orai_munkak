2766a93b7c427c6e937de6ab81f23d517062e043
András Bercsek <berandris@gmail.com>
2023-09-06 20:14:50 +0200

    gggg

--- autok2/autok2/Program.cs
+++ autok2/autok2/Program.cs
@@ -29,6 +29,15 @@ namespace autok2
 
         }
 
+        public string Rendszam
+        {
+
+            get { return rendszam; }
+
+            set { rendszam = value; }
+
+        }
+
     }
 
     class Auto
@@ -53,6 +62,15 @@ namespace autok2
 
         }
 
+        public string Rendszam
+        {
+
+            get { return rendszam; }
+
+            set { rendszam = value; }
+
+        }
+
     }
 
     class Program
@@ -60,10 +78,49 @@ namespace autok2
         static void Main(string[] args)
         {
 
+            /*
             Car c = new Car();
-
             Auto a = new Auto();
+            */
 
+            Random veletlen = new Random();
+
+            Car[] autok1 = new Car[5];
+            Auto[] autok2 = new Auto[5];
+
+            for (int i = 0; i < autok1.Length; i++)
+            {
+                char c = (char)(veletlen.Next(0, 26) + 65);
+                autok1[i].Sebesseg = veletlen.Next(1, 6);
+                
+                autok1[i].Rendszam = "";
+                autok1[i].Rendszam += c;
+                autok1[i].Rendszam += "-";
+                autok1[i].Rendszam += autok1[i].Sebesseg;
+
+                autok2[i]= new Auto();
+                autok2[i].Sebesseg = veletlen.Next(1, 6);
+
+                
+
+            }
+            //foreach (Car a in autok1)
+            //{
+            //    Console.WriteLine(a.Rendszam);
+            //}
+
+            Console.SetCursorPosition(40, 7);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.Write(autok1[3].Rendszam);
+            Console.ReadKey(true);
+            Console.Clear();
+
+            Console.SetCursorPosition(40 - autok1[3].Sebesseg, 7);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Write(autok1[3].Rendszam);
+            Console.ReadKey();
         }
     }
 }
