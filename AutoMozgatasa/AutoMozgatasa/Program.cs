deab749b8c75788ce217216d77ac5805cec8d0e8
András Bercsek <berandris@gmail.com>
2023-09-13 18:40:30 +0200

    09, 13,

---
+++ AutoMozgatasa/AutoMozgatasa/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMozgatasa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"({Console.BufferWidth}X{Console.BufferHeight})");
+            /*Console.WindowLeft = 50;
+            Console.WindowTop = 20;
+            Console.SetWindowSize(70,40);
+            Console.BufferWidth = 70;
+            Console.BufferHeight = 40;
+            //for (int sor = 1; sor <= 40; sor++)    
+            {
+                for (int oszlop = 1; oszlop <= 70; oszlop++)
+                {
+                    Console.Write("+");
+                }
+            }
+            Console.WriteLine("Bálint Dezső (c) 2023");
+            Console.Write("Nyomj egy billentyűt a törléshez...");
+            Console.ReadKey(true);
+            Console.Clear();
+            //Console.ReadKey();
+            System.Threading.Thread.Sleep(3000);*/
+            String auto = "A-1";
+            /*Console.SetCursorPosition(Console.WindowWidth - 3, 31);
+            Console.Write(auto);
+            System.Threading.Thread.Sleep(10000);*/
+            int poz = Console.WindowWidth - 3;
+            while (true)
+            {
+                Console.Clear();
+                /*try
+                {
+                    Console.SetCursorPosition(poz, 10);
+                }catch(Exception e)
+                {
+                    break;
+                }*/
+                Console.SetCursorPosition(poz, 10);
+                Console.Write(auto);
+                if (poz > 3)
+                {
+                    poz -= 3;
+                }
+                else
+                {
+                    break;
+                }
+                System.Threading.Thread.Sleep(1000/16);
+            }
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}
---
+++ adatfeldolg1/adatfeldolg1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adatfeldolg1
+{
+
+    struct Zene
+    {
+
+        int ado;
+        int perc;
+        int masodperc;
+        string eloado;
+        string cim;
+
+
+        public int Ado {
+            get
+            {
+                return ado;
+            }
+        }
+
+        public int Perc
+        {
+            get
+            {
+                return perc;
+            }
+        }
+        public int Masodperc
+        {
+            get
+            {
+                return masodperc;
+            }
+        }
+        public string Eloado
+        {
+            get
+            {
+                return eloado;
+            }
+        }
+        public string Cim
+        {
+            get
+            {
+                return cim;
+            }
+        }
+
+        public Zene(string sor)
+        {
+
+            string[] splitSor = sor.Split(':');
+
+            cim = splitSor[1];
+            string[] split2Sor = splitSor[0].Split(' ');
+
+            ado = int.Parse(split2Sor[0]);
+            perc = int.Parse(split2Sor[1]);
+            masodperc = int.Parse(split2Sor[2]);
+            //eloado = split2Sor[3];
+            eloado = "";
+            for (int i = 3; i < split2Sor.Length; i++)
+            {
+
+                eloado += split2Sor[i] + " ";
+                
+            }
+            eloado = eloado.Trim();
+        }
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Zene> z = new List<Zene> ();
+
+            StreamReader reader = new StreamReader("zenek.txt");
+
+            while (!reader.EndOfStream)
+            {
+
+                Zene zenecske = new Zene (reader.ReadLine());
+                z.Add(zenecske);
+
+            }
+
+            Console.ReadKey();
+            reader.Close();
+
+
+        }
+    }
+}
