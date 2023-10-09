38182edfdde732255a4192d049278423cc430025
András Bercsek <berandris@gmail.com>
2023-10-09 20:19:38 +0200

    aaaa

---
+++ delegalas/delegalas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegalas
+{
+    internal class Program
+    {
+        delegate int Muvelet(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Szamolo.Szorzas(2, 3));
+
+            Muvelet m1 = Szamolo.Szorzas;
+            Console.WriteLine(m1(12, 12));
+
+            Muvelet m2 = (new Szamolo()).Hatvanyozas;
+            Console.WriteLine(m2(2, 12));
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+
+
+    class Szamolo
+    {
+        public static int Szorzas(int szorzando, int szorzo)
+        {
+            int szorzat = szorzando;
+
+            for (int db = 1; db < szorzo; db++)
+            {
+                szorzat += szorzando;
+            }
+
+            return szorzat;
+        }
+
+        public int Hatvanyozas(int alap, int kitevo)
+        {
+            int hatvany = 1;
+
+            for (int db = 1; db <= kitevo; db++)
+            {
+                hatvany *= alap;
+            }
+
+            return hatvany;
+        }
+
+        public static void Hatvany(int alap, int kitevo)
+        {
+
+        }
+    }
+}
---
+++ robotos/robotos/Robot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace robotos
+{
+    delegate void MitTud(string tudas);
+    internal class Robot
+    {
+        char[] tudas;
+        public MitTud aztCsinal;
+        Button fizimiska;
+
+        public Robot() 
+        {
+            fizimiska = new Button();
+            fizimiska.Text = "Robika";
+            fizimiska.Left = (new Random().Next(10, 100));
+            fizimiska.Top = (new Random().Next(10, 100));
+        }
+    }
+}
