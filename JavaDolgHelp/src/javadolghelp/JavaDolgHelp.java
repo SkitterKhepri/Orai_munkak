4f4541c3db1f318bdd58cf8861bf63fdb52277f7
Voldemario <VoldeMario>
2024-01-15 18:58:04 +0100

    talán hasznos

---
+++ JavaDolgHelp/src/javadolghelp/JavaDolgHelp.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javadolghelp;
+
+/**
+ *
+ * @author Diak
+ */
+public class JavaDolgHelp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        UjFrame ablak = new UjFrame();
+        ablak.setVisible(true);
+        
+        Masik a2 = new Masik();
+        a2.setVisible(true);
+        a2.setSize(600, 800);
+        
+    }
+    
+}
